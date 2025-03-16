aa1c50553a18a226cacaa144131fe0309e905082
thanhtuandev <pro03tuan@gmail.com>
2025-03-16 08:24:05 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program {
+    static int So(int a, int b){
+        return a+b;
+    }
+    static void Inra(){
+        Console.WriteLine("Xin chao");
+    }
+    static void Main() {
+        So(5,6);
+        Inra();
+    }
+}
